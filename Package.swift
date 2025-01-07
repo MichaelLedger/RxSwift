3a07ab41723cadec5a3f1b9a21503c4c7e93ee51
GavinXiang <gavin.xiang@planetart.cn>
2025-01-07 11:03:56 +0800

    feat: fix spm clang: error: no such file or directory in dynamic frameworks

--- Package.swift
+++ Package.swift
@@ -32,12 +32,28 @@ extension Target {
       return [.rxTarget(name: "RxCocoa", dependencies: ["RxSwift", "RxRelay", "RxCocoaRuntime"])]
     #endif
   }
+    
+  static func rxCocoaDynamic() -> [Target] {
+    #if os(Linux)
+      return [.rxTarget(name: "RxCocoa-Dynamic", dependencies: ["RxSwift", "RxRelay"])]
+    #else
+      return [.rxTarget(name: "RxCocoa-Dynamic", dependencies: ["RxSwift", "RxRelay", "RxCocoaRuntime"])]
+    #endif
+  }
 
   static func rxCocoaRuntime() -> [Target] {
     #if os(Linux)
       return []
     #else
       return [.rxTarget(name: "RxCocoaRuntime", dependencies: ["RxSwift"])]
+    #endif
+  }
+    
+  static func rxCocoaRuntimeDynamic() -> [Target] {
+    #if os(Linux)
+      return []
+    #else
+      return [.rxTarget(name: "RxCocoaRuntime-Dynamic", dependencies: ["RxSwift"])]
     #endif
   }
 
@@ -60,24 +76,30 @@ let package = Package(
       .library(name: "RxRelay", targets: ["RxRelay"]),
       .library(name: "RxBlocking", targets: ["RxBlocking"]),
       .library(name: "RxTest", targets: ["RxTest"]),
-      .library(name: "RxSwift-Dynamic", type: .dynamic, targets: ["RxSwift"]),
-      .library(name: "RxCocoa-Dynamic", type: .dynamic, targets: ["RxCocoa"]),
-      .library(name: "RxRelay-Dynamic", type: .dynamic, targets: ["RxRelay"]),
-      .library(name: "RxBlocking-Dynamic", type: .dynamic, targets: ["RxBlocking"]),
-      .library(name: "RxTest-Dynamic", type: .dynamic, targets: ["RxTest"]),
+      .library(name: "RxSwift-Dynamic", type: .dynamic, targets: ["RxSwift-Dynamic"]),
+      .library(name: "RxCocoa-Dynamic", type: .dynamic, targets: ["RxCocoa-Dynamic"]),
+      .library(name: "RxRelay-Dynamic", type: .dynamic, targets: ["RxRelay-Dynamic"]),
+      .library(name: "RxBlocking-Dynamic", type: .dynamic, targets: ["RxBlocking-Dynamic"]),
+      .library(name: "RxTest-Dynamic", type: .dynamic, targets: ["RxTest-Dynamic"]),
     ],
     Product.allTests()
   ] as [[Product]]).flatMap { $0 },
   targets: ([
     [
       .rxTarget(name: "RxSwift", dependencies: []),
+      .rxTarget(name: "RxSwift-Dynamic", dependencies: []),
     ],
     Target.rxCocoa(),
+    Target.rxCocoaDynamic(),
     Target.rxCocoaRuntime(),
+    Target.rxCocoaRuntimeDynamic(),
     [
       .rxTarget(name: "RxRelay", dependencies: ["RxSwift"]),
       .target(name: "RxBlocking", dependencies: ["RxSwift"]),
       .target(name: "RxTest", dependencies: ["RxSwift"]),
+      .rxTarget(name: "RxRelay-Dynamic", dependencies: ["RxSwift"]),
+      .target(name: "RxBlocking-Dynamic", dependencies: ["RxSwift"]),
+      .target(name: "RxTest-Dynamic", dependencies: ["RxSwift"]),
     ],
     Target.allTests()
   ] as [[Target]]).flatMap { $0 },
